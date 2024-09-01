556cdaeceebc450aca27f5fe7a20b09f8ee59d15
anujabhang <abhang.apm@gmail.com>
2024-09-02 00:13:45 +0530

    Add a profile to repository

--- src/main/java/com/anuj/match_ai_backend/MatchAiBackendApplication.java
+++ src/main/java/com/anuj/match_ai_backend/MatchAiBackendApplication.java
@@ -1,13 +1,40 @@
 package com.anuj.match_ai_backend;
 
+import com.anuj.match_ai_backend.entities.Profiles.Gender;
+import com.anuj.match_ai_backend.entities.Profiles.Profile;
+import com.anuj.match_ai_backend.repositories.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MatchAiBackendApplication {
+public class MatchAiBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	ProfileRepository profileRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatchAiBackendApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		Profile profile = new Profile(
+			"1",
+				"Anuj",
+				"Abhang",
+				21,
+				"Mongolian",
+				Gender.Male,
+				"Love to Travel",
+				"hi.jpg",
+				"INTP"
+
+		);
+
+		profileRepository.save(profile);
+		profileRepository.findAll().forEach(System.out::println);
+	}
 }
---
+++ src/main/java/com/anuj/match_ai_backend/entities/Profiles/Profile.java
@@ -0,0 +1,16 @@
+package com.anuj.match_ai_backend.entities.Profiles;
+
+public record Profile(
+        String id,
+        String firstName,
+        String lastName,
+        int age,
+        String ethnicity,
+        Gender gender,
+        String bio,
+        String imageUrl,
+        String myersBriggsPersonalityType
+) {
+
+
+}
