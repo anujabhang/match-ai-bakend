dc96b0ed46ab8329a09040acb5b029bb59a11aef
anujabhang <abhang.apm@gmail.com>
2024-09-01 19:31:45 +0530

    Initial commit

---
+++ src/main/java/com/anuj/match_ai_backend/MatchAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anuj.match_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuj/match_ai_backend/MatchAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuj.match_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
